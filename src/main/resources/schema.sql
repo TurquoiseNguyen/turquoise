4ac4e3c871bf5f9067cda0e972934d6a3b5809f2
hanhchinhxyz <hanhchinh.xyz@gmail.com>
2019-07-31 23:30:52 +0700

    c

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,15 +1,9 @@
-DROP TABLE IF EXISTS app_user CASCADE;
+DROP TABLE IF EXISTS jp_users CASCADE;
 CREATE TABLE jp_users(
 	id BIGINT(20) NOT NULL PRIMARY KEY,
 	user_login VARCHAR(60),
 	user_pass VARCHAR(64),
-	user_nicename VARCHAR(50),
-	user_email VARCHAR(100),
-	user_url VARCHAR(100),
-	user_registered TIMESTAMP,
-	user_activation_key VARCHAR(60),
-	user_status SMALLINT,
-	display_name VARCHAR(250)
+	user_nicename VARCHAR(50)
 );
 
 CREATE UNIQUE INDEX user_login ON jp_users(user_login);
