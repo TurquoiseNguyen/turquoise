4f33eb9440339045900dbde211932e447b1e2dcb
hanhchinhxyz <hanhchinh.xyz@gmail.com>
2019-07-31 23:14:56 +0700

    fix new schema user

--- src/main/java/com/turquoise/web/entities/User.java
+++ src/main/java/com/turquoise/web/entities/User.java
@@ -10,6 +10,7 @@ import javax.persistence.*;
  *
  */
 @Entity
+@Table(name="jp_users")
 public class User implements Serializable{
 
 	/**
@@ -22,27 +23,6 @@ public class User implements Serializable{
 	@SequenceGenerator(name="user_seq_gen", sequenceName="user_seq", allocationSize=1)
     @Column(name="id", nullable=false)
     private Long id;
-    
-	@Column(name = "name", length = 36, nullable=false)
-    private String name;
-	
-    @Column(name = "first_name", length = 36)
-    private String firstName;
-    
-    @Column(name = "mid_name", length = 100)
-    private String midName;
-    
-    @Column(name = "last_name", length = 36)
-    private String lastName;
-    
-    @Column(name = "email", length = 200, nullable = false)
-    private String email;
-    
-    @Column(name = "emailVerified")
-    private boolean emailVerified;
-    
-    @Column(name = "encryted_password", length = 128, nullable = false)
-    private String password;
 
 	public Long getId() {
 		return id;
@@ -50,64 +30,6 @@ public class User implements Serializable{
 
 	public void setId(Long id) {
 		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getMidName() {
-		return midName;
-	}
-
-	public void setMidName(String midName) {
-		this.midName = midName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public boolean isEmailVerified() {
-		return emailVerified;
-	}
-
-	public void setEmailVerified(boolean emailVerified) {
-		this.emailVerified = emailVerified;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-    
-    
-
+	}   
+	
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,73 +1,19 @@
 DROP TABLE IF EXISTS app_user CASCADE;
-CREATE TABLE app_user(
-	id BIGINT not null PRIMARY KEY,
-	name VARCHAR(36) not null unique,
-	first_name VARCHAR(36),
-	last_name VARCHAR(36),
-	mid_name VARCHAR(100),
-	email VARCHAR(200) not null unique,
-	emailVerified boolean,
-	encryted_password VARCHAR(128) not null
+CREATE TABLE jp_users(
+	id BIGINT(20) not null PRIMARY KEY,
+	user_login VARCHAR(60),
+	user_pass VARCHAR(64),
+	user_nicename VARCHAR(50),
+	user_email VARCHAR(100),
+	user_url VARCHAR(100),
+	user_registered TIME,
+	user_activation_key VARCHAR(60),
+	user_status INT(11),
+	display_name VARCHAR(250)
 );
 
-DROP TABLE IF EXISTS app_role CASCADE;
-CREATE TABLE app_role(
-	id BIGINT not null PRIMARY KEY,
-	name VARCHAR(30) not null unique
-);
+CREATE UNIQUE INDEX user_login ON jp_users(user_login);
 
-DROP TABLE IF EXISTS user_role CASCADE;
-CREATE TABLE user_role(
-	id BIGINT not null PRIMARY KEY,
-	user_id BIGINT not null REFERENCES app_user(id),
-	role_id BIGINT not null REFERENCES app_role(id)
-);
+CREATE UNIQUE INDEX user_nicename ON jp_users(user_nicename);
 
-DROP TABLE IF EXISTS oauth2_token CASCADE;
-CREATE TABLE oauth2_token(
-	id BIGINT not null PRIMARY KEY,
-	token VARCHAR(255) not null,
-	provider VARCHAR(8) not null,
-	user_id BIGINT not null REFERENCES app_user(id)
-);
-
-DROP TABLE IF EXISTS password_reset_token CASCADE;
-CREATE TABLE password_reset_token(
-	id BIGINT not null PRIMARY KEY,
-	expiry_date timestamp not null,
-	token VARCHAR(255) not null unique,
-	user_id BIGINT not null REFERENCES app_user(id)
-);
-
-DROP TABLE IF EXISTS config CASCADE;
-CREATE TABLE config(
-	id BIGINT not null PRIMARY KEY,
-	name VARCHAR(36) not null unique,
-	val VARCHAR(200) not null
-);
-
-DROP TABLE IF EXISTS region_type CASCADE;
-CREATE TABLE region_type(
-	id BIGINT not null PRIMARY KEY,
-	name VARCHAR(100) not null,
-	call VARCHAR(100) null,
-	level SMALLINT not null
-);
-
-DROP TABLE IF EXISTS region CASCADE;
-CREATE TABLE region(
-	id BIGINT not null PRIMARY KEY,
-	name VARCHAR(100) not null,
-	fname VARCHAR(200) not null,
-	type_id BIGINT REFERENCES region_type(id),
-	superior_id BIGINT REFERENCES region(id)
-);
-
-DROP TABLE IF EXISTS persistent_logins CASCADE;
-CREATE TABLE persistent_logins (
-	series varchar(64) not null PRIMARY KEY,
-    username varchar(64) not null,
-    token varchar(64) not null,
-    last_used timestamp not null
-);
 
